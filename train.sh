c4f1cd17b8d99a9a96a85f6b9209711489382f18
RiyaoDong <89683997+RiyaoDong@users.noreply.github.com>
2021-08-28 17:15:24 +0800

    Create train.sh

---
+++ train.sh
@@ -0,0 +1,4 @@
+python train.py --gpu=0 --data_name CUB --top_k=3 --batch_size=84
+python train.py --gpu=0 --data_name CAR --top_k=3 --batch_size=84
+python train.py --gpu=0 --data_name FLOWERS --top_k=3 --batch_size=84
+python train.py --gpu=0 --data_name FGVC --top_k=3 --batch_size=84
